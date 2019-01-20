0cc8dfb8fa4fcab29eab6ea5e5d425239383c8a3
Adi Narayana Vemuru <vemuruadi@gmail.com>
2019-01-20 14:22:27 +0530

    small change to test ci

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 const port = process.env.PORT || 3000
-const startMsg = 'Server is running on port - ' + port
+const startMsg = 'Server is running on port ' + port
 
 app.get('/', (req, res) => {
     console.log('API sucessfully called')
